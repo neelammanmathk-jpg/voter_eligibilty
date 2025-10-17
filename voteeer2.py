e0396ca88597dc1463225099b61238b758d6c67c
Khaleel <01fe24ica06@kletech.ac.in>
2025-10-17 10:48:42 +0530

    Edited Kartik's file

---
+++ voteeer2.py
@@ -0,0 +1,6 @@
+age = int(input("Enter the age: "))  # Convert input to integer
+
+if age > 18:  # No parentheses needed around condition
+    print("Eligible to vote")  # Indent inside if block
+else:
+    print("Not eligible")  # Indent inside else block and add colon after else
